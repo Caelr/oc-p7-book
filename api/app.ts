62d2dd7cd0c7f2135e29a6dbc1a14a19e0afca83
Rachidi Judicaël <wahab@MacBook-Pro-de-Rachidi.local>
2023-10-24 19:02:25 +0400

    feat: app.ts

---
+++ api/app.ts
@@ -0,0 +1,35 @@
+import express from 'express'
+
+import { deserializedUser } from './middleware/deserializedUser'
+import routes from './routes'
+import { config } from './utils/config'
+import connect from './utils/connect'
+import log from './utils/logger'
+import path from 'path'
+
+const app = express()
+
+const port = config.PORT
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  )
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+  next()
+})
+
+
+app.use(express.json())
+
+app.use('/images', express.static(path.join(__dirname, '/images')))
+
+app.use(deserializedUser)
+
+app.listen(port, async () => {
+  log.info(`App is running at http://localhost:${port}`)
+  await connect()
+  routes(app)
+})
